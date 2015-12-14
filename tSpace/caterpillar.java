19f95936a9fb959cb82301e5fec6f3d2745c46c1
egmr <erey774@aucklanduni.ac.nz>
2015-12-15 12:40:55 +1300

    Modified for simulation

--- tSpace/caterpillar.java
+++ tSpace/caterpillar.java
@@ -11,30 +11,82 @@ public class caterpillar {
 		String[] t2string = {"3","4","1","2","5"};
 		double[] t2height = {2,2,6,8,10};
 		double time = 0.1;
-		boolean iterate = true;  //if true: time is the increment and prints to file;
-		//if false: time is desired single point in time - doesn't print to file yet
+		boolean iterate = true;
+		boolean distances = false;
+		boolean absSpeed = false;
 		
 		String filename = "NewickFormatTrees.txt";
 		
-		tNode.generate(t1string, t1height, t2string, t2height, time, iterate, filename);			
 		
-		//For Attempt1
-		/*
-		ArrayList<tNode> t1 = tNode.generateTree(t1string, t1height);
-		ArrayList<tNode> t2 = tNode.generateTree(t2string, t2height);
-		System.out.println(t1);
-		System.out.println(t2);
-		ArrayList<tNode> tdiff = tNode.difference(t1,t2,time);		
-		ArrayList<tNode> treeAtTime = tNode.sortAndGenerateTree(tdiff);		
-		System.out.println(treeAtTime);
-		System.out.println();	
-		
-		//Shows change from t1 to t2 over increments of time;
-		for(double i=0; i <= 1; i+=0.2){
-			ArrayList<tNode> k = tNode.difference(t1, t2, i);
-			ArrayList<tNode> z = tNode.sortAndGenerateTree(k);
-			System.out.println(z);
+		boolean treeOk = true;
+		while (treeOk) {
+			t1height = generateRandomTreeHeights(4);
+			t2height = generateRandomTreeHeights(4);
+			treeOk = areTreesOk(t1height, t2height);
 		}
-		*/		
+		
+		for(int i = 0; i<4; i++){
+			System.out.println("Tree1 : " + t1height[i] + "   Tree2 : " + t2height[i]);
+			
+		}
+		int t1Move = 2; //indicates speed for t1 from stage 1-3 (inc) - possible speeds are 0-3
+		int t2Move = 0; //indicates speed of t2 at stage1 (although currently 0 is the only option here);
+		int t3Move = 3; //indicates speed of t3 at stage 2-3 (inc) - possible speeds are 0-3
+		
+		
+		double minDistance = 100.0;
+		int bestT1 = 0;
+		int bestT3 = 0;
+		int[] otherT1 = new int[16];
+		int[] otherT3 = new int[16];
+		double[] otherDistance = new double[16];
+		int counter = 0;
+		//t1Move = i, t2Move = j
+		for(int i = 0; i < 4; i++){			
+			for(int j = 0; j<4; j++){
+				double dist = tNode.generate(t1string, t1height, t2string, t2height, time,
+						iterate, distances, absSpeed, filename, i, t2Move, j);
+				if(dist < minDistance){
+					minDistance = dist;
+					bestT1 = i;
+					bestT3 = j;
+				} else if (dist == minDistance){
+					otherT1[counter] = i;
+					otherT3[counter] = j;
+					otherDistance[counter] = dist;
+					counter++;
+				}
+			}
+		}
+		System.out.println("The choice of speeds: " + bestT1 + " and " + bestT3 + " found the shortest path.");
+		System.out.println("The distance of this path is: " + minDistance);
+		for(int i = 0; i< otherDistance.length; i++){
+			if(otherDistance[i] == minDistance){
+				System.out.println("Other same speed Choices: " + otherT1[i] + " , " + otherT3[i]);
+			}
+		}		
+	}
+	public static double[] generateRandomTreeHeights(int length){
+		Random r = new Random();
+		double[] treeHeights = new double[length];
+		double count = 0;
+		for(int i = 1; i < 4 ; i++){
+			count += r.nextDouble();
+			treeHeights[i] = count;
+		}
+		treeHeights[0] = treeHeights[1];
+		return treeHeights;
+	}	
+	//checks to see if node of 4 reaches node3 before(1,2) (based on fastest t1 can go)
+	public static boolean areTreesOk(double[] tree1, double[] tree2){
+		double d1 = tree1[2] - tree1[1];
+		double d2 = Math.abs(tree2[3] - tree1[1]);
+		double d3 = tree1[3] - tree1[2];
+		double d4 = Math.abs(tree1[3] - tree2[1]);
+		
+		
+		//if it returns false the trees are ok -- bad readability
+		return d1/d2 < d3/d4;
+	}		
 	}
 }
